932604b85cbc7d5b96f1d1a656751c2b7e485d1d
Keigo Hashimoto <hashimoto.keigo@moneyforward.co.jp>
2026-04-16 16:32:59 +0900

    Finish layout and routes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,9 +7,8 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # 指定のワーカー数でテストを並列実行する
   parallelize(workers: :number_of_processors)
-
   # test/fixtures/*.ymlにあるすべてのfixtureをセットアップする
   fixtures :all
-
+  include ApplicationHelper
   # （すべてのテストで使うその他のヘルパーメソッドは省略）
 end
