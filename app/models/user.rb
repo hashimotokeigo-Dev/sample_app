d4c06b437abe9b810189748c7abe99533a85c09b
Keigo Hashimoto <hashimoto.keigo@moneyforward.co.jp>
2026-05-25 16:44:34 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save :downcase_email
@@ -79,6 +80,12 @@ class User < ApplicationRecord
   # パスワードの再設定の期限が切れている場合はtrueを返す
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where(user_id: id)
   end
 
   private
